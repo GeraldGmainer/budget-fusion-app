ae706a171502c0b0bf2cb4d3fc21ace9282e7045
Gerald Gmainer <gerald.gmainer@designium.jp>
2024-11-24 22:03:48 +0100

    refactoring

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,5 +1,3 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
-
 export 'cache/cache.dart';
 export 'constants/constants.dart';
 export 'di/injection.dart';
@@ -8,5 +6,3 @@ export 'enums/enums.dart';
 export 'exceptions/exceptions.dart';
 export 'service/service.dart';
 export 'supabase/supabase.dart';
-
-final supabase = Supabase.instance.client;
