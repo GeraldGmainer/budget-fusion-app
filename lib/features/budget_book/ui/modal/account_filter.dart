9071d5f655354dc801e8860b285ec7c062c79623
Gerald Gmainer <gerald.gmainer@designium.jp>
2025-03-27 00:27:48 +0100

    account filter uses cubit

--- lib/features/budget_book/ui/modal/account_filter.dart
+++ lib/features/budget_book/ui/modal/account_filter.dart
@@ -1,24 +1,32 @@
+import 'package:budget_fusion_app/core/core.dart';
+import 'package:budget_fusion_app/features/account/account.dart';
 import 'package:budget_fusion_app/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../domain/entities/budget_book_filter.dart';
 
 class AccountFilter extends StatelessWidget {
-  final List<String> _accounts = ['All Accounts', 'Cash', 'Credit Card'];
   final BudgetBookFilter filter;
 
-  AccountFilter({super.key, required this.filter});
+  const AccountFilter({super.key, required this.filter});
 
   @override
   Widget build(BuildContext context) {
-    return FilterChipGroup(
-      title: 'Accounts',
-      items: _accounts,
-      // selectedItem: filter.account,
-      selectedItem: 'All Accounts',
-      onItemSelected: (value) {
-        // TODO filter by account
-        // filter.account = value;
+    // TODO dont use account cubit because of DDD ? or used shared AccountCubit
+    return BlocBuilder<AccountCubit, LoadableState>(
+      builder: (context, state) {
+        return state.maybeWhen(
+            loaded: (data) => FilterChipGroup(
+                  title: 'Accounts',
+                  items: data,
+                  selectedItem: filter.account,
+                  valueToString: (data) => data?.name ?? "Unknown",
+                  onItemSelected: (Account? value) {
+                    filter.account = value;
+                  },
+                ),
+            orElse: () => SizedBox());
       },
     );
   }
