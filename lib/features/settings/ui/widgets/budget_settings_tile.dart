cf7e15a83859e90825f667a428501043e0e047a8
Gerald Gmainer <gerald.gmainer@designium.jp>
2025-05-20 23:17:07 +0200

    setting screen

---
+++ lib/features/settings/ui/widgets/budget_settings_tile.dart
@@ -0,0 +1,61 @@
+import 'package:budget_fusion_app/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+typedef BudgetSettingsTileCallback = void Function(BuildContext context);
+
+class BudgetSettingsTile extends AbstractSettingsTile {
+  final IconData icon;
+  final String title;
+  final String? description;
+  final String? route;
+  final BudgetSettingsTileCallback? onPressed;
+  final String? value;
+  final ValueChanged<bool>? onToggleChanged;
+  final Color? iconColor;
+
+  const BudgetSettingsTile.navigation({super.key, required this.icon, required this.title, required this.route, this.description, this.iconColor})
+    : onPressed = null,
+      value = null,
+      onToggleChanged = null;
+
+  const BudgetSettingsTile.action({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.onPressed,
+    this.description,
+    this.value,
+    this.onToggleChanged,
+    this.iconColor,
+  }) : route = null;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget? trailingWidget;
+
+    if (route != null) {
+      trailingWidget = const Icon(Icons.chevron_right, color: AppColors.accentColor);
+    } else if (value != null && value!.isNotEmpty) {
+      trailingWidget = Text(value!, style: TextStyle(color: AppColors.secondaryTextColor, fontSize: 16));
+    }
+
+    VoidCallback? tapCallback;
+
+    if (route != null) {
+      tapCallback = () => Navigator.of(context).pushNamed(route!);
+    } else if (onPressed != null) {
+      tapCallback = () => onPressed!(context);
+    }
+
+    return SettingsTile(
+      leading: Icon(icon, color: iconColor ?? AppColors.accentColor),
+      title: Text(title),
+      description: description != null ? Text(description!) : null,
+      trailing: trailingWidget,
+      onPressed: (ctx) {
+        tapCallback?.call();
+      },
+    );
+  }
+}
--- lib/shared/ui/drawer/drawer_item.dart
+++ lib/shared/ui/drawer/drawer_item.dart
@@ -1,23 +1,24 @@
 import 'package:budget_fusion_app/core/core.dart';
 import 'package:budget_fusion_app/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AppDrawerItem extends StatelessWidget {
   final IconData icon;
   final String title;
   final String? route;
   final bool isSelected;
+  final Color? color;
+  final String? url;
 
-  const AppDrawerItem({
-    super.key,
-    required this.icon,
-    required this.title,
-    required this.isSelected,
-    required this.route,
-  });
+  const AppDrawerItem({super.key, required this.icon, required this.title, required this.isSelected, required this.route, this.color, this.url});
 
   void _onTap(BuildContext context) {
     Scaffold.of(context).closeDrawer();
+    if (url != null) {
+      launchUrl(Uri.parse(url!), mode: LaunchMode.externalApplication);
+      return;
+    }
     if (route != null) {
       final currentRoute = ModalRoute.of(context)?.settings.name;
       if (currentRoute != route) {
@@ -31,17 +32,8 @@ class AppDrawerItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Icon(
-        icon,
-        color: isSelected ? AppColors.accentColor : null,
-      ),
-      title: Text(
-        title,
-        style: TextStyle(
-          color: isSelected ? AppColors.accentColor : null,
-          fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-        ),
-      ),
+      leading: Icon(icon, color: isSelected ? Theme.of(context).colorScheme.secondary : (color ?? Colors.white70)),
+      title: Text(title, style: TextStyle(color: isSelected ? AppColors.accentColor : null, fontWeight: isSelected ? FontWeight.bold : FontWeight.normal)),
       selected: isSelected,
       onTap: () => _onTap(context),
     );
