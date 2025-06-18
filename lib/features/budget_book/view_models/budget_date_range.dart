3129a4dfd76fe4f0197d00d99daf647ead1ac154
Gerald Gmainer <gerald.gmainer@designium.jp>
2025-06-18 14:03:40 +0200

    improved log

--- lib/features/budget_book/view_models/budget_date_range.dart
+++ lib/features/budget_book/view_models/budget_date_range.dart
@@ -14,4 +14,10 @@ class BudgetDateRange with _$BudgetDateRange {
   factory BudgetDateRange.all() => BudgetDateRange(period: PeriodMode.all, from: DateTime.fromMillisecondsSinceEpoch(0), to: DateTime.fromMillisecondsSinceEpoch(9999999999999));
 
   bool contains(DateTime date) => !date.isBefore(from) && !date.isAfter(to);
+
+  @override
+  String toString() {
+    String f(DateTime d) => '${d.year.toString().padLeft(4, '0')}-${d.month.toString().padLeft(2, '0')}-${d.day.toString().padLeft(2, '0')}';
+    return 'BudgetDateRange(period: $period, from: ${f(from)}, to: ${f(to)})';
+  }
 }
